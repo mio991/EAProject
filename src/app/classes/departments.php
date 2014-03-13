b0d00f019be542615d61768c906f4a8e15d9783b
BendekiSen <bendeki.sen@live.de>
2014-03-13 12:47:02 +0100

    Update departments.php

--- src/app/classes/departments.php
+++ src/app/classes/departments.php
@@ -1,1 +1,33 @@
-bla
+<?php
+	
+	class departments {
+
+		
+		function __construct($params = null) {
+			// connect to db
+
+		}
+
+		private $defaults = array(
+			'id' => null,
+			'name' => null,
+			'min_count' => null
+		);
+
+		static public function read () {
+
+		}
+
+		public function create () {
+
+		}
+
+		public function update () {
+
+		}
+
+		static public function delete (id) {
+
+		}
+	}
+?>
